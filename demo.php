67cdffd822ac2401830a688317bb710a34fa3d45
marchtea <marchtea213@gmail.com>
2013-11-08 02:46:59 +0800

    test some code generate rss complete

--- demo.php
+++ demo.php
@@ -1,5 +1,72 @@
 <?php
 require_once('crawler.php');
+
+function simplexml_import_xml($parent, $xml, $before = false)
+{
+	$xml = (string)$xml;
+
+	// check if there is something to add
+	if ($nodata = !strlen($xml) or $parent[0] == NULL) {
+		return $nodata;
+	}
+
+	// add the XML
+	$node     = dom_import_simplexml($parent);
+	$fragment = $node->ownerDocument->createDocumentFragment();
+	$fragment->appendXML($xml);
+
+	if ($before) {
+		return (bool)$node->parentNode->insertBefore($fragment, $node);
+	}
+
+	return (bool)$node->appendChild($fragment);
+}
+/*
+ *  insert SimpleXMLElement into SimpleXMLElement
+ *
+ *  * @param SimpleXMLElement $parent
+ *   * @param SimpleXMLElement $child
+ *    * @param bool $before
+ *     * @return bool SimpleXMLElement added
+ *      */
+function simplexml_import_simplexml($parent, $child, $before = false)
+{
+	// check if there is something to add
+	if ($child[0] == NULL) {
+		return true;
+	}
+
+	// if it is a list of SimpleXMLElements default to the first one
+	$child = $child[0];
+
+	// insert attribute
+	if ($child->xpath('.') != array($child)) {
+		$parent[$child->getName()] = (string)$child;
+		return true;
+	}
+
+	$xml = $child->asXML();
+
+	// remove the XML declaration on document elements
+	if ($child->xpath('/*') == array($child)) {
+		$pos = strpos($xml, "\n");
+		$xml = substr($xml, $pos + 1);
+	}
+
+	return simplexml_import_xml($parent, $xml, $before);
+}
+
+//extend SimpleXML support addCData
+class SimpleXMLExtended extends SimpleXMLElement {
+	public function addCData($cdata_text) {
+		$node = dom_import_simplexml($this); 
+		$no   = $node->ownerDocument; 
+		$node->appendChild($no->createCDATASection($cdata_text)); 
+	} 
+}
+
+
+
 class GeneralCrawler extends CrawlJob
 {
 	private $rssfilename;
@@ -135,47 +202,80 @@ class GeneralCrawler extends CrawlJob
 	public function jobDone($crawler)
 	{
 		echo " ?? all job done\n";
-		var_dump($this->urlArray);
+		var_dump($this->urlArray[0]);
 
-		/*
-		//because of simplexml not supporting <xxx:xxx> tag, preprocess first
-		$xmlstr = file_get_contents($this->rssfilename);
+		$xmlstr = file_get_contents("template.rss");
+		$xmlstr = preg_replace('/<atom:link/', '<atomlink', $xmlstr);
+		$xmlstr = preg_replace('/<\/atom:link>/', '</atomlink>', $xmlstr);
 		$xmlstr = preg_replace('/<content:encoded>/', '<content_encoded>', $xmlstr);
 		$xmlstr = preg_replace('/<\/content:encoded>/', '</content_encoded>', $xmlstr);
 		$xmlstr = preg_replace('/<dc:creator>/', '<dc_creator>', $xmlstr);
 		$xmlstr = preg_replace('/<\/dc:creator>/', '</dc_creator>', $xmlstr);
 		$xmlstr = preg_replace('/<dc:creator\/>/', '<dc_creator/>', $xmlstr);
 		$xml = simplexml_load_string($xmlstr, 'SimpleXMLExtended', LIBXML_NOCDATA);
+		$channel = $xml->xpath('/rss/channel');
+		$channel = $channel[0];
+		$channel->lastBuildDate = date('r');
+		$olditems = $channel->xpath('item');
 
-		$items = $xml->xpath('/rss/channel/item');
+		$rule = $this->urlArray[0]->rule;
+		$channel->title = $rule['title'];
+		$channel->link = $rule['url'];
+		$channel->description = $rule['description'];
+		$channel->atomlink->attributes()->href = $rule['url'];
+		unset($channel->item);
 
-		$newjobs = array();
-
-		uasort($this->results, array('self', 'listCmp'));
-		foreach($this->results as $k =>$result)
+		foreach($rule['items'] as $item)
 		{
-			foreach($result as $key => $res)
+			//in list
+			if (array_key_exists('autoRefer', $item) && strcmp($item['autoRefer'], 'true') == 0)
 			{
-				if (strcmp($key, 'ref') == 0)
-					continue;
-				//echo 'url:  '.$res['url']."\n";
-				$result[$key]['url'] = 'http://software.hit.edu.cn'.$res['url'];
-				foreach($items as $item)
+				$ru = $item['rule'];
+				foreach($ru['url'] as $num => $url)
 				{
-					if (strcmp($item->link, $result[$key]['url']) == 0)
+					$newnode = $channel->addChild('item');			
+					$newnode->addChild('link', $url);
+					$newnode->addChild('guid', $url);
+
+					foreach($ru['items'] as $it)
 					{
-						echo 'url:'.$result[$key]['url']."\n";
-						unset($result[$key]);
-						break;
+						$title = $newnode->addChild($it['as']);
+						$title->addCData($it['value'][$num]);
 					}
 				}
+			}else if (array_key_exists('value', $item))//has value
+			{
+				foreach($item['value'] as $value)
+				{
+					$newnode = $channel->addChild('item');			
+					$title = $newnode->addChild('title');
+					$title->addCData($item['dom']);
+					//$newnode->title = '<![CDATA[ '.$result['title'].' ]]>';
+					//$newnode->addChild(new SimpleXMLElement('<item><title></title></item>'));
+					$newnode->addChild('link', $item['url']);
+					$newnode->addChild('guid', $item['url']);
+
+					$descnote = $newnode->addChild('description');
+					$descnote->addCData($value);
+					$content = $newnode->addChild('content_encoded');
+					$content->addCData($value);
+					$newnode->addChild('dc_creator', $result['admin']);
+				}
+
 			}
-			$itemjob = new SoftwareItemJob($result, $this->rssfilename);
-			$newjobs[] = $itemjob;
 		}
-		$crawler->addJobs($newjobs);
-		 */
-		//echo "add done\n";
+
+		$xmlstr = $xml->asXML();
+		$xmlstr = preg_replace('/<content_encoded>/', '<content:encoded>', $xmlstr);
+		$xmlstr = preg_replace('/<\/content_encoded>/', '</content:encoded>', $xmlstr);
+		$xmlstr = preg_replace('/<dc_creator>/', '<dc:creator>', $xmlstr);
+		$xmlstr = preg_replace('/<\/dc_creator>/', '</dc:creator>', $xmlstr);
+		$xmlstr = preg_replace('/<dc_creator\/>/', '<dc:creator/>', $xmlstr);
+		file_put_contents($this->rssfilename, $xmlstr);
+		echo "file: $this->rssfilename saved\n";	
+
+
+
 	}
 	public static function listCmp($a, $b)
 	{
@@ -194,7 +294,7 @@ class GeneralCrawler extends CrawlJob
 
 date_default_timezone_set('Asia/Shanghai');
 
-$rulestr = '{"url":"http://software.hit.edu.cn/article/list.aspx", "title":"软件学院通知", "items":[{"dom":"ul.page_news_list li a", "autoRefer":"true", "operation":"property.href", "rule":{"items":[{"dom": "h3.page_news_title", "operation":"property.innertext"}]}}]}';
+$rulestr = '{"url":"http://software.hit.edu.cn/article/list.aspx", "title":"软件学院通知","description": "test",  "items":[{"dom": "div.grid_12 p", "operation": "property.innertext"},{"dom":"ul.page_news_list li a", "autoRefer":"true", "operation":"property.href", "rule":{"items":[{"dom": "h3.page_news_title", "operation":"property.innertext", "as": "title"},{"dom": "i.page_news_date", "operation": "property.innertext", "as": "description"}]}}]}';
 
 $rule = json_decode($rulestr, true);
 var_dump($rule);
